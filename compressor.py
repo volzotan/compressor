16f9c9be21e83ecfab41bcecc56c8245a331495b
Christopher Getschmann <christopher@volzo.de>
2016-07-02 02:15:12 +0200

    compressor now uses an own config file

--- compressor.py
+++ compressor.py
@@ -6,22 +6,16 @@ from stacker import Stacker
 import os
 import sys
 import support
+import yaml
 
-INPUT_DIR_ALIGNER   = "export"
-TRANSLATION_DATA    = "translation_data.json"
-OUTPUT_DIR_ALIGNER  = "aligned"
+import config
 
-NAMING_PREFIX       = "m18"
-INPUT_DIR_STACKER   = OUTPUT_DIR_ALIGNER
-OUTPUT_DIR_STACKER  = "stack_" + NAMING_PREFIX + "2"
-EXTENSION           = ".jpg"
 
 def print_config():
     pass
 
+
 def path_check_and_expand(path):
-
-
     if not (path.startswith("/") or path.startswith("~")):
         path = os.path.join(BASE_DIR, path)
 
@@ -30,69 +24,81 @@ def path_check_and_expand(path):
 
     return path
 
+
 def create_if_not_existing(path):
     if not os.path.exists(path):
         print("created directoy: {}".format(path))
         os.makedirs(path)
 
+
+def abort_if_missing(path):
+    if not os.path.exists(path):
+        print("dir not found: {}".format(support.Color.BOLD + path + support.Color.END))
+        sys.exit(-1)
+
+
+def get_all_file_names(input_dir):
+    file_list = []
+    for root, dirs, files in os.walk(input_dir):
+        for f in files:
+
+            if f == ".DS_Store":
+                continue
+
+            if not f.lower().endswith(EXTENSION):
+                continue
+
+            if os.path.getsize(os.path.join(INPUT_DIR, f)) < 100:
+                continue
+
+            file_list.append(f)
+
+
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
 aligner = Aligner()
 stacker = Stacker()
-input_images = []
+input_images_aligner = []
+input_images_stacker = []
 
 # transform to absolute paths
 BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 # --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
-INPUT_DIR_ALIGNER   = path_check_and_expand(INPUT_DIR_ALIGNER)
-INPUT_DIR_STACKER   = path_check_and_expand(INPUT_DIR_STACKER)
-OUTPUT_DIR_STACKER  = path_check_and_expand(OUTPUT_DIR_STACKER)
-OUTPUT_DIR_ALIGNER  = path_check_and_expand(OUTPUT_DIR_ALIGNER)
-TRANSLATION_DATA    = path_check_and_expand(TRANSLATION_DATA)
+# expand all paths
+for directory in config.DIRS_TO_CHECK:
+    variable = getattr(config, directory)
+    setattr(config, directory, path_check_and_expand(variable))
 
-if not os.path.exists(INPUT_DIR):
-    print("INPUT_DIR not found: {}".format(support.Color.BOLD + INPUT_DIR + support.Color.END))
-    sys.exit(-1)
+for directory in config.DIRS_ABORT_IF_MISSING:
+    abort_if_missing(directory)
 
-create_if_not_existing(OUTPUT_DIR_STACKED)
-create_if_not_existing(OUTPUT_DIR_ALIGNED)
+for directory in config.DIRS_TO_CREATE:
+    create_if_not_existing(directory)
 
-# get all file names
-for root, dirs, files in os.walk(INPUT_DIR):
-    for f in files:
-
-        if f == ".DS_Store":
-            continue
-
-        if not f.lower().endswith(EXTENSION):
-            continue
-
-        if os.path.getsize(os.path.join(INPUT_DIR, f)) < 100:
-            continue
-
-        input_images.append(f)
+input_images_aligner = get_all_file_names(config.INPUT_DIR_ALIGNER)
+input_images_stacker = get_all_file_names(config.INPUT_DIR_STACKER)
 
 # print len(input_images)
 
 # init aligner
 aligner.REFERENCE_IMAGE                 = "/Users/volzotan/Desktop/export/DSC03135.jpg"
-aligner.INPUT_DIR                       = INPUT_DIR_ALIGNER
-aligner.OUTPUT_DIR                      = OUTPUT_DIR_ALIGNER
-aligner.EXTENSION                       = EXTENSION
-aligner.TRANSLATION_DATA                = TRANSLATION_DATA
+aligner.INPUT_DIR                       = config.INPUT_DIR_ALIGNER
+aligner.OUTPUT_DIR                      = config.OUTPUT_DIR_ALIGNER
+aligner.EXTENSION                       = config.EXTENSION
+aligner.TRANSLATION_DATA                = config.TRANSLATION_DATA
 aligner.USE_CORRECTED_TRANSLATION_DATA  = True
 
 #aligner.init()
 #aligner.step2()
 
 # init stacker
-stacker.INPUT_DIRECTORY     = INPUT_DIR_STACKER
-stacker.NAMING_PREFIX       = NAMING_PREFIX
-stacker.RESULT_DIRECTORY    = OUTPUT_DIR_STACKER
+stacker.INPUT_DIRECTORY     = config.INPUT_DIR_STACKER
+stacker.NAMING_PREFIX       = config.NAMING_PREFIX
+stacker.RESULT_DIRECTORY    = config.OUTPUT_DIR_STACKER
 stacker.BASE_DIR            = BASE_DIR
-stacker.EXTENSION           = EXTENSION
+stacker.EXTENSION           = config.EXTENSION
 
 stacker.print_config()
 stacker.run(input_images)
